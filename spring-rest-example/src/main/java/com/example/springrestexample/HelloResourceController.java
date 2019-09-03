c958f9980fa630de2503e5539a8ad60b79dfd0be
pattabhia <shivaram2004@gmail.com>
2019-09-04 05:28:07 +0530

    adding new file
    adding new file

--- spring-rest-example/src/main/java/com/example/springrestexample/HelloResourceController.java
+++ spring-rest-example/src/main/java/com/example/springrestexample/HelloResourceController.java
@@ -1,8 +1,11 @@
 package com.example.springrestexample;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/hello")
@@ -11,5 +14,10 @@ public class HelloResourceController {
     @GetMapping
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping(value = "/json", produces = APPLICATION_JSON_VALUE)
+    public Hello helloJson() {
+        return new Hello("Greetings", "Hello World");
     }
 }
