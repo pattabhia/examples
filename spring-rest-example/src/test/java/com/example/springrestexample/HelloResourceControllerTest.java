d106d0a591760da76461e9d88f95900bf2d3a8da
pattabhia <shivaram2004@gmail.com>
2019-09-04 05:23:26 +0530

    adding test

--- spring-rest-example/src/test/java/com/example/springrestexample/HelloResourceControllerTest.java
+++ spring-rest-example/src/test/java/com/example/springrestexample/HelloResourceControllerTest.java
@@ -1,16 +1,17 @@
 package com.example.springrestexample;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class HelloResourceControllerTest {
@@ -31,5 +32,14 @@ public class HelloResourceControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
     }
+
+    @Test
+    public void testHelloWorldJson() throws Exception {
+        mockMvc.perform(get("/hello/json").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", Matchers.is("Greetings")))
+                .andExpect(jsonPath("$.value",Matchers.is("Hello World")));
+    }
+
 
 }
