b24a73686e1299dbd86eb4f022a913961fcf88ec
pattabhia <shivaram2004@gmail.com>
2019-09-05 22:51:16 +0530

    refactoring
    refactoring

--- spring-rest-example/src/test/java/com/example/springrestexample/HelloResourceControllerTest.java
+++ spring-rest-example/src/test/java/com/example/springrestexample/HelloResourceControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -33,7 +32,7 @@ public class HelloResourceControllerTest {
                 .andExpect(content().string("hello"));
     }
 
-    
+
     @Test
     public void testHelloWorldJson() throws Exception {
         mockMvc.perform(get("/hello/json").accept(MediaType.APPLICATION_JSON))
