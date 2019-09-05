e2b32db62bb65fce744cb8c5c0e8af0bc65271d5
pattabhia <shivaram2004@gmail.com>
2019-09-05 22:52:50 +0530

    refactoring
    refactoring

--- spring-rest-example/src/test/java/com/example/springrestexample/SpringRestExampleApplicationTests.java
+++ spring-rest-example/src/test/java/com/example/springrestexample/SpringRestExampleApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringRestExampleApplicationTests {
 
+
     @Test
     public void contextLoads() {
     }
