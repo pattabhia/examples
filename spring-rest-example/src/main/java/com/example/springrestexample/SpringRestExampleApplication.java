4d413f45b4be188548d4bbf6688562ac6388dddc
pattabhia <shivaram2004@gmail.com>
2019-09-08 22:39:44 +0530

    adding author
    adding author

--- spring-rest-example/src/main/java/com/example/springrestexample/SpringRestExampleApplication.java
+++ spring-rest-example/src/main/java/com/example/springrestexample/SpringRestExampleApplication.java
@@ -2,7 +2,7 @@ package com.example.springrestexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//author:pattabhi
 @SpringBootApplication
 public class SpringRestExampleApplication {
 
