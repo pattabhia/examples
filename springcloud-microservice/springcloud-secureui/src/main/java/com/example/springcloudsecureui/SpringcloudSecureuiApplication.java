a932afabfaca3f3c1b8539d397a575ee136a7124
pattabhia <shivaram2004@gmail.com>
2020-01-17 06:12:28 +0530

    adding spring secureui module
    adding spring secureui module

---
+++ springcloud-microservice/springcloud-secureui/src/main/java/com/example/springcloudsecureui/SpringcloudSecureuiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudsecureui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudSecureuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudSecureuiApplication.class, args);
+    }
+
+}
---
+++ springcloud-microservice/springcloud-secureui/src/test/java/com/example/springcloudsecureui/SpringcloudSecureuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudsecureui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudSecureuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
