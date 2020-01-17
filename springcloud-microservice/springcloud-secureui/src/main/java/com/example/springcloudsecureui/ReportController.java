fd279dfa403f3d681bc8749101a666e16f85977c
pattabhia <shivaram2004@gmail.com>
2020-01-17 06:18:52 +0530

    adding request mapping
    adding request mapping

--- springcloud-microservice/springcloud-secureui/src/main/java/com/example/springcloudsecureui/ReportController.java
+++ springcloud-microservice/springcloud-secureui/src/main/java/com/example/springcloudsecureui/ReportController.java
@@ -1,4 +1,13 @@
 package com.example.springcloudsecureui;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ReportController {
+
+    @RequestMapping("/home")
+    public String loadHome() {
+        return "home";
+    }
 }
