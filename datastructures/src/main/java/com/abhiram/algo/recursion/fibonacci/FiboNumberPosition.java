8ac8a2983c453071733790487ff875896ed7c257
pattabhia <shivaram2004@gmail.com>
2020-04-11 23:10:05 +0530

    Update FiboNumberPosition.java

--- datastructures/src/main/java/com/abhiram/algo/recursion/fibonacci/FiboNumberPosition.java
+++ datastructures/src/main/java/com/abhiram/algo/recursion/fibonacci/FiboNumberPosition.java
@@ -15,7 +15,9 @@ public class  FiboNumberPosition {
         if (num == 1 || num == 2) {
             return num-1;
         }
+
         return fibonacci(num - 1) + fibonacci(num - 2);
+
     }
 }
 
