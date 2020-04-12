03813ca267c8c1e55c828a223751e19ebf21cfc7
pattabhia <shivaram2004@gmail.com>
2020-04-12 23:15:41 +0530

    adding logic for alternative fib series
    adding logic for alternative fib series

---
+++ datastructures/src/main/java/com/abhiram/algo/recursion/fibonacci/AlternateFibonacciSeries.java
@@ -0,0 +1,8 @@
+package com.abhiram.algo.recursion.fibonacci;
+
+public class AlternateFibonacciSeries {
+
+    public static void main(String[] args) {
+        //TODO: Alternate Fibonacci series
+    }
+}
