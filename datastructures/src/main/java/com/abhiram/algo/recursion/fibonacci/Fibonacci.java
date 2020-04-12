304a4784ac4e8a63da6bbce987abf30f3f158b04
pattabhia <shivaram2004@gmail.com>
2020-04-12 23:03:37 +0530

    separating logic

--- datastructures/src/main/java/com/abhiram/algo/recursion/fibonacci/Fibonacci.java
+++ datastructures/src/main/java/com/abhiram/algo/recursion/fibonacci/Fibonacci.java
@@ -1,5 +1,20 @@
 package com.abhiram.algo.recursion.fibonacci;
 
-public interface Fibonacci {
+public abstract class Fibonacci {
 
+    public int fibonacci(int num) {
+        if (num == 0 ) {
+            return 0;
+        }
+        if (num == 1 || num == 2) {
+            return 1;
+        }
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+
+    static long nextFibonacci(int n)
+    {
+        double a = n * (1 + Math.sqrt(5)) / 2.0;
+        return Math.round(a);
+    }
 }
