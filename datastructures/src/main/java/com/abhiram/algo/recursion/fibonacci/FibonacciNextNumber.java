b2da25606fbb1ec26324ce09e6afba110c2cb3e3
pattabhia <shivaram2004@gmail.com>
2020-04-13 07:44:15 +0530

    Fix: To check if a input number is Fibonnaci or not!

--- datastructures/src/main/java/com/abhiram/algo/recursion/fibonacci/FibonacciNextNumber.java
+++ datastructures/src/main/java/com/abhiram/algo/recursion/fibonacci/FibonacciNextNumber.java
@@ -3,10 +3,12 @@ package com.abhiram.algo.recursion.fibonacci;
 public class FibonacciNextNumber extends Fibonacci {
 
     // Driver code
-    public static void main (String[] args)
-    {
-        int n = 8;
+    public static void main(String[] args) {
+        int n = 6;
+        if (!isFibonacci(n)) {
+            System.err.println("You have entered non-fibonacci number to fetch next the fib number!");
+            return;
+        }
         System.out.printf("The next fib number of %d is %d", n, nextFibonacci(n));
     }
-    //TODO: Error if input number is not fib number.
 }
