0a782c082a9d8d8ee3b31172bc5daca9e466be0b
pattabhia <shivaram2004@gmail.com>
2020-04-13 07:17:17 +0530

    code formatting

--- datastructures/src/main/java/com/abhiram/algo/recursion/fibonacci/AlternateFibonacciSeries.java
+++ datastructures/src/main/java/com/abhiram/algo/recursion/fibonacci/AlternateFibonacciSeries.java
@@ -3,6 +3,12 @@ package com.abhiram.algo.recursion.fibonacci;
 public class AlternateFibonacciSeries {
 
     public static void main(String[] args) {
-        //TODO: Alternate Fibonacci series
+        for (int i = 0; i <= 10; ++i) {
+            //if i%2==0, it is even numbers.
+            // i%2>0, it is odd number.
+            if (i % 2 > 0) {
+                System.out.println(new FibonacciSeries().fibonacci(i));
+            }
+        }
     }
 }
